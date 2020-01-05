ecc4e760c21e829e2fb9b7e310017cdcd3b5063d
Ignacio Keegan <46688572+RailGun15@users.noreply.github.com>
2020-01-05 01:24:32 -0300

    Add files via upload

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,23 @@
+def player_to_dict(player):
+    return {
+        "number":player[0],
+        "position":player[1],
+        'name': player[2],
+        'date_of_birth': player[3],
+        'caps': player[4],
+        'club': player[5],
+        'country': player[6],
+        'club_country': player[7],
+        'year': player[8]
+    }
+    
+
 def players_by_position(squads_list):
-    pass
+    new_dict = {}
+    for player in squads_list:
+        position = str(player[1])
+        if position not in new_dict:
+            new_dict[position] = [player_to_dict(player)]
+        else:
+            new_dict[position].append(player_to_dict(player))
+    return new_dict
--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,8 @@
+from football_dictionaries.assignment_2 import players_by_position,player_to_dict
 def players_by_country_and_position(squads_list):
-    pass
+    import itertools
+    new_dict = {}
+    for key, group in itertools.groupby(squads_list, key = lambda item: item[6]):
+        new_dict[key] = players_by_position(group)
+        
+    return new_dict
